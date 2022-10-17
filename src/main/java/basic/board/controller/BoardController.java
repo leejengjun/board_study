d6426a30d942754ce2d0e9b931dd2265489c2a8f
Jeongjun.lee <youngwoo4215@naver.com>
2022-10-17 18:54:46 +0900

    게시판 - 글쓰기 - 상세조회 - 수정

--- src/main/java/basic/board/controller/BoardController.java
+++ src/main/java/basic/board/controller/BoardController.java
@@ -4,12 +4,15 @@ import basic.board.domain.dto.BoardDto;
 import basic.board.service.BoardService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Fetch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -19,21 +22,72 @@ public class BoardController {
 
     private BoardService boardService;
 
+    /**
+     * 게시물 리스트 보기
+     */
     @GetMapping("/list")
-    public String boardList() {
+    public String boardList(Model model) {
+        List<BoardDto> boardList = boardService.getBoardList();
+
+        model.addAttribute("boardList", boardList);
         return "board/list.html";
     }
 
+    /**
+     * 글쓰기 
+     */
     @GetMapping("/post")
     public String boardWrite() {
         return "board/write.html";
     }
 
+    /**
+     * 글쓰기 완료
+     */
     @PostMapping("/post")
     public String boardWrite(BoardDto boardDto) {
         boardService.savePost(boardDto);
         log.info("info boardDto={}", boardDto);
         return "redirect:/board/list";
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * 게시글 상세조회 페이지
+     */
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+        
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    /**
+     * 게시글 수정 페이지
+     */
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+    /**
+     * 게시글 수정완료
+     */
+    @PatchMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+
+        return "redirect:/board/list";
+    }
+
+    /**
+     * 게시글 삭제
+     */
+
 
 }
--- src/main/java/basic/board/service/BoardService.java
+++ src/main/java/basic/board/service/BoardService.java
@@ -1,11 +1,16 @@
 package basic.board.service;
 
 import basic.board.domain.dto.BoardDto;
+import basic.board.domain.entity.BoardEntity;
 import basic.board.repository.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,5 +22,43 @@ public class BoardService {
     @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardList() {
+
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for (BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDto);
+        }
+
+        return boardDtoList;
+
+    }
+
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDto;
     }
 }
